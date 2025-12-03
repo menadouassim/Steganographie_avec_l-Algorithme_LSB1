8b8ddbc1ae25a52c6cec921e6f393db341e4fb63
Administrateur <administrateur@Mac.lan>
2025-12-03 19:03:59 +0100

    feature: convert picture's pixel  values to even number function

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PIL import Image
 
 # Open an image file
-im = Image.open("marvels-test-image.png")
-print(im.format, im.size, im.mode)
+test_image = Image.open("marvels-test-image.png").convert("RGB")
+print(test_image.format, test_image.size, test_image.mode)
 
 
 """ 
@@ -13,7 +13,7 @@ def get_pixel_value(image, x, y):
     pixel_value = image.getpixel((x, y))
     return pixel_value
     
-print(get_pixel_value(im, 10, 30))
+print(get_pixel_value(test_image, 10, 30))
 """
 """
 #function to convert string to binary
@@ -26,3 +26,24 @@ print(convert_to_binary("Hello"))
 
 
 
+"""
+"""
+
+#function to to turn each pixel value in a picture into even value
+def convert_image_values_to_even_numbers(image):
+    pixels = image.load()
+    width, height = image.size
+    for y_position in range(height):
+        for x_position in range(width):
+            red_value, green_value, blue_value = pixels[x_position, y_position]
+
+            # Convert each color to an even value
+            red_value = red_value if red_value % 2 == 0 else red_value - 1
+            green_value = green_value if green_value % 2 == 0 else green_value - 1
+            blue_value = blue_value if blue_value % 2 == 0 else blue_value - 1
+            pixels[x_position, y_position] = (red_value, green_value, blue_value)
+
+            modified_image=image.save("even_image.png")
+    return modified_image
+
+convert_image_values_to_even_numbers(test_image)
