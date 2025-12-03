50d42936986b4b36f45857e58b4cf9965995e949
Administrateur <administrateur@MacBook-Air-de-Ouassim.local>
2025-12-03 15:49:51 +0100

    feature : get pixel values function

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ print(im.format, im.size, im.mode)
 """ 
 """
 
+#function to get pixel values 
+def get_pixel_value(image, x, y):
+    pixel_value = image.getpixel((x, y))
+    return pixel_value
+    
+print(get_pixel_value(im, 10, 30))
+
+
+
+
+
