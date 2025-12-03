f59d97c5690e0223008055d01b13ba7ae037ed51
Administrateur <administrateur@MacBook-Air-de-Ouassim.local>
2025-12-03 15:55:12 +0100

    feature : convert text to binary function

--- main.py
+++ main.py
@@ -14,7 +14,14 @@ def get_pixel_value(image, x, y):
     return pixel_value
     
 print(get_pixel_value(im, 10, 30))
+"""
+"""
+#function to convert string to binary
+def convert_to_binary(string):
+    binary_equivilant = ' '.join(format(ord(char), '08b') for char in string)
+    return binary_equivilant
 
+print(convert_to_binary("Hello"))
 
 
 
